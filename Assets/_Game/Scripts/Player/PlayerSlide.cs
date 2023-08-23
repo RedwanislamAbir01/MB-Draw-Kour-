87f19e334c4a3bbb5d1f2ce7556980319d0fdd4c
Redwan Islam <62832246+RedwanislamAbir01@users.noreply.github.com>
2023-08-23 10:45:07 +0600

    slide anim fix

--- Assets/_Game/Scripts/Player/PlayerSlide.cs
+++ Assets/_Game/Scripts/Player/PlayerSlide.cs
@@ -6,6 +6,7 @@ namespace _Game
     public class PlayerSlide : MonoBehaviour
     {
         [Header("References")]
+        [SerializeField] private LineFollower _lineFollower;
         [SerializeField] private Animator _animator;
         [SerializeField] private Transform _visual;
 
@@ -34,44 +35,99 @@ namespace _Game
             {
                 if (hitInfo.transform.TryGetComponent(out ISlideable slideable))
                 {
-                    InitiateSlide();
+                    if (_lineFollower.IsWayPointAvailable())
+                    {
+                        if (CheckIfPointBetweenObjects(transform, hitInfo.transform, _lineFollower.LastWayPoint()))
+                        {
+                            const float distance = 2.5f;
+                            var newPoint = _lineFollower.LastWayPoint() + transform.forward * distance;
+                            _lineFollower.AddToWayPoint(newPoint);
+                        }
+                        else
+                        {
+                            if (hitInfo.collider.bounds.Contains(_lineFollower.LastWayPoint()))
+                            {
+                                // Debug.Log("Inside");
+                                const float distance = 1.5f;
+                                var newPoint = _lineFollower.LastWayPoint() + transform.forward * distance;
+                                _lineFollower.AddToWayPoint(newPoint);
+                            }
+
+
+                            else
+                            {
+                                InitiateSlide();
+                            }
+
+                        }
+                    }
                 }
             }
         }
 
-        private void InitiateSlide()
-        {
-            _isSliding = true;
+            private bool CheckIfPointBetweenObjects(Transform objectA, Transform objectB, Vector3 point)
+            {
+                var directionVector = objectB.position - objectA.position;
+                var vectorToObjectA = point - objectA.position;
+                var vectorToObjectB = point - objectB.position;
 
-            PlayerState.Instance.SetState(PlayerState.State.Parkour);
+                var dotProductA = Vector3.Dot(directionVector, vectorToObjectA);
+                var dotProductB = Vector3.Dot(directionVector, vectorToObjectB);
 
-            _animator.SetTrigger(_Slide);
-
-            SlideVisualEffect();
-        }
-
-        private void SlideVisualEffect()
-        {
-            const float slideDuration = 0.1f;
-            const float slideDownDistance = -0.8f;
-            const float slideUpDelay = .8f;
-            const float slideUpDuration = 0.3f;
-
-            _visual.DOLocalMoveY(slideDownDistance, slideDuration)
-                .OnComplete(() =>
+                if (dotProductA > 0 && dotProductB < 0)
                 {
-                    _visual.DOLocalMoveY(0f, slideUpDuration).SetDelay(slideUpDelay)
-                        .OnComplete(CompleteSlide);
-                });
+                    // Debug.Log("Point is between the objects.");
+                    return true;
+                }
+
+                // Debug.Log("Point is NOT between the objects.");
+                return false;
+            }
+
+
+            private bool CanStartSlide()
+            {
+                // Add any additional conditions to check if sliding can start
+                return true; // For example, you might want to check if the player is grounded.
+            }
+
+            private void InitiateSlide()
+            {
+                _isSliding = true;
+
+                PlayerState.Instance.SetState(PlayerState.State.Parkour);
+
+                _animator.SetTrigger(_Slide);
+
+                SlideVisualEffect();
+            }
+
+            private void SlideVisualEffect()
+            {
+                const float slideDuration = 0.1f;
+                const float slideDownDistance = -0.8f;
+                const float slideUpDelay = .8f;
+                const float slideUpDuration = 0.3f;
+
+                _visual.DOLocalMoveY(slideDownDistance, slideDuration)
+                    .OnComplete(() =>
+                    {
+                        _visual.DOLocalMoveY(0f, slideUpDuration).SetDelay(slideUpDelay)
+                            .OnComplete(CompleteSlide);
+                    });
+            }
+
+            private void CompleteSlide()
+            {
+                _isSliding = false;
+
+                PlayerState.Instance.SetState(PlayerState.State.Default);
+
+            if (_lineFollower.IsWayPointAvailable())
+            {
+                _animator.SetTrigger(_Run);
+            }
         }
-
-        private void CompleteSlide()
-        {
-            _isSliding = false;
-
-            PlayerState.Instance.SetState(PlayerState.State.Default);
-
-            _animator.SetTrigger(_Run);
         }
     }
-}
+
