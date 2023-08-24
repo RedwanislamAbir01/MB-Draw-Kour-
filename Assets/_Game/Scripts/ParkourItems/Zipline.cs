17c117b41e9728fc9b3cf69a7af52703058cc51a
Pranta Protik <pranta.cseku@gmail.com>
2023-08-24 13:36:23 +0600

    Zipline handle interaction added

--- Assets/_Game/Scripts/ParkourItems/Zipline.cs
+++ Assets/_Game/Scripts/ParkourItems/Zipline.cs
@@ -7,13 +7,21 @@ namespace _Game
         [SerializeField] private Transform _startPoint;
         [SerializeField] private Transform _endPoint;
         [SerializeField] private Transform _dropPoint;
-        [SerializeField] private float _duration = 2f;
+        [SerializeField] private float _jumpDuration = 1f;
+        [SerializeField] private float _zipDuration = 2f;
+        [SerializeField] private GameObject _startHandle;
+        [SerializeField] private GameObject _endHandle;
+
+        private void Awake()
+        {
+            _endHandle.SetActive(false);
+        }
         
         private void OnTriggerEnter(Collider other)
         {
             if(other.transform.TryGetComponent(out PlayerZipLining playerZipLining))
             {
-                playerZipLining.ZipLine(_startPoint, _endPoint, _dropPoint, _duration);
+                playerZipLining.ZipLine(_startPoint, _endPoint, _dropPoint, _jumpDuration, _zipDuration, _startHandle, _endHandle);
             }
         }
     }
--- Assets/_Game/Scripts/Player/PlayerZipLining.cs
+++ Assets/_Game/Scripts/Player/PlayerZipLining.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Game
@@ -9,28 +10,40 @@ namespace _Game
         [SerializeField] private Animator _animator;
         [SerializeField] private LineFollower _lineFollower;
         [SerializeField] private Vector3 _camOffset;
+        [SerializeField] private GameObject _playerHandle;
         
         private static readonly int _Ziplining = Animator.StringToHash("Ziplining");
         private static readonly int _Idle = Animator.StringToHash("Idle");
         private static readonly int _Run = Animator.StringToHash("Run");
 
-        public void ZipLine(Transform startPoint, Transform endPoint, Transform dropPoint, float duration)
+        private void Awake()
+        {
+            _playerHandle.SetActive(false);
+        }
+
+        public void ZipLine(Transform startPoint, Transform endPoint, Transform dropPoint, float jumpDuration, float zipDuration, GameObject startHandle, GameObject endHandle)
         {
             _lineFollower.ClearLine();
             _animator.SetTrigger(_Ziplining);
             PlayerState.Instance.SetState(PlayerState.State.Parkour);
             CamManager.Instance.SetFollowCamTrackedObjectOffset(_camOffset);
             
-            const float moveDuration = 0.1f;
-            transform.DORotate(Vector3.zero, moveDuration);
-            transform.DOMove(startPoint.position, moveDuration).OnComplete(() =>
+            const float rotationDuration = 0.1f;
+            transform.DORotate(Vector3.zero, rotationDuration);
+            transform.DOMove(startPoint.position, jumpDuration).OnComplete(() =>
             {
-                transform.DOMove(endPoint.position, duration).SetEase(Ease.Linear).OnComplete(() =>
+                startHandle.SetActive(false);
+                _playerHandle.SetActive(true);
+                
+                transform.DOMove(endPoint.position, zipDuration).SetEase(Ease.Linear).OnComplete(() =>
                 {
                     _animator.ResetTrigger(_Run);
                     _animator.SetTrigger(_Idle);
                     
-                    transform.DOMove(dropPoint.position, moveDuration).OnComplete(() =>
+                    endHandle.SetActive(true);
+                    _playerHandle.SetActive(false);
+                    
+                    transform.DOMove(dropPoint.position, jumpDuration).OnComplete(() =>
                     {
                         PlayerState.Instance.SetState(PlayerState.State.Default);
                         CamManager.Instance.EnableStartCam();
